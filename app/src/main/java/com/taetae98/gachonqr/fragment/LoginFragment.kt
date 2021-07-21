227dfc603adc35bf18c1f0d2cbfb505dd4d801ef
KangTaeJong98 <rkdxowhd98@naver.com>
2021-07-21 15:34:31 +0900

    QR-3 LoginFragment 디자인 만들기

--- app/src/main/java/com/taetae98/gachonqr/fragment/LoginFragment.kt
+++ app/src/main/java/com/taetae98/gachonqr/fragment/LoginFragment.kt
@@ -1,8 +1,22 @@
 package com.taetae98.gachonqr.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.taetae98.gachonqr.R
 import com.taetae98.gachonqr.databinding.BindingFragment
 import com.taetae98.gachonqr.databinding.FragmentLoginBinding
 
 class LoginFragment : BindingFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        onCreateSupportActionBar()
+
+        return binding.root
+    }
+
+    private fun onCreateSupportActionBar() {
+        setSupportActionBar(binding.toolbar)
+    }
 }
